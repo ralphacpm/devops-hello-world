a2090b272277a3896cabe5f77d6f9580fc373944
Ralph <macalino47@gmail.com>
2025-12-27 11:00:24 +0800

    Automated the deployment after git push

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello! I am updated automatically with jenkins!"
+    return "Hello! One git push is all it takes!"
 
 if __name__ == '__main__':
     # run() method of Flask class runs the application
